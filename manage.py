a32fe6cf3814fe045e11c8b50d418afc70eaf351
David Sundararaj <dev.davidsundararaj@gmail.com>
2017-08-31 18:57:36 +0530

    Minor changes

--- manage.py
+++ manage.py
@@ -14,35 +14,35 @@ class CreateDB(Command):
         if not path.exists(path.join(path.dirname(__file__), 'app', 'db')):
             makedirs(path.join(path.dirname(__file__), 'app', 'db'))
 
-        from app import db
-        db.create_all()
-        subs = ["Computer Networks", "Unix Internals", "Digital Communications",
-                "Digital Principles and Design", "Computer Architecture",
-                "Digital Signal Processing", "Engineering Graphics",
-                "Operating Systems", "Discrete Mathematics"]
+            from app import db
+            db.create_all()
+            subs = ["Computer Networks", "Unix Internals", "Digital Communications",
+                    "Digital Principles and Design", "Computer Architecture",
+                    "Digital Signal Processing", "Engineering Graphics",
+                    "Operating Systems", "Discrete Mathematics"]
 
-        p = mimesis.Personal(locale='en')
-        profs = [p.full_name() for i in range(20)]
+            p = mimesis.Personal(locale='en')
+            profs = [p.full_name() for i in range(20)]
 
-        for i in subs:
-            db.session.add(SubjectsNames(name=i))
+            for i in subs:
+                db.session.add(SubjectsNames(name=i))
 
-        for i in profs:
-            db.session.add(ProfessorsNames(name=i))
+            for i in profs:
+                db.session.add(ProfessorsNames(name=i))
 
-        for i in range(1, 9):
-            for j in range(randint(5, 7)):
-                db.session.add(Subjects(sem=i, sub=choice(subs), prof=choice(profs)))
+            for i in range(1, 9):
+                for j in range(randint(5, 7)):
+                    db.session.add(Subjects(sem=i, sub=choice(subs), prof=choice(profs)))
 
-        for name in profs:
-            db.session.add(
-                Professors(
-                    name=name, spec=choice(subs),
-                    qual=choice(["Master's", "PhD"]) + " in " + choice(subs),
-                    exp=randint(2, 20)))
+            for name in profs:
+                db.session.add(
+                    Professors(
+                        name=name, spec=choice(subs),
+                        qual=choice(["Master's", "PhD"]) + " in " + choice(subs),
+                        exp=randint(2, 20)))
 
-        db.session.add(Login(rollno=00000000, email="admin@admin.com", password="password", isadmin=1))
-        db.session.commit()
+            db.session.add(Login(rollno=00000000, email="admin@admin.com", password="password", isadmin=1))
+            db.session.commit()
 
 
 if __name__ == "__main__":
