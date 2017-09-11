4d8a15f92ef660816d1f54f9591998ed1cbc89d2
D4D3VD4V3 <dev.davidsundararaj@gmail.com>
2017-09-11 08:53:14 +0530

    Moar build automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require("gulp");
 var gshell = require("gulp-shell")
 
-gulp.task("activatevenv", gshell.task(["source ~/.virtualenvs/venv/bin/activate"]))
+//gulp.task("activatevenv", gshell.task(["source ~/.virtualenvs/venv/bin/activate"]))
 gulp.task("runtests", gshell.task(["pytest"]))
+gulp.task("updaterequirements", gshell.task(["pip freeze > requirements.txt", "echo updated requirements"]))
 
 gulp.task("watch", function(){
     gulp.watch(["./app/**/*.py"], ["runtests"]);});
 
-gulp.task("default", ["runtests", "watch"]);
+gulp.task("default", ["watch", "runtests", "updaterequirements"]);
