ac45a125ca29d4df99218f5bca285808f04fdfb5
D4D3VD4V3 <dev.davidsundararaj@gmail.com>
2017-09-11 08:57:35 +0530

    Minor refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require("gulp");
+var gulp = require("gulp")
 var gshell = require("gulp-shell")
 
 //gulp.task("activatevenv", gshell.task(["source ~/.virtualenvs/venv/bin/activate"]))
 gulp.task("runtests", gshell.task(["pytest"]))
-gulp.task("updaterequirements", gshell.task(["pip freeze > requirements.txt", "echo updated requirements"]))
+gulp.task("updaterequirements", gshell.task(["pip freeze > requirements.txt"]))
 
 gulp.task("watch", function(){
-    gulp.watch(["./app/**/*.py"], ["runtests"]);});
+    gulp.watch(["./app/**/*.py"], ["runtests"])})
 
-gulp.task("default", ["watch", "runtests", "updaterequirements"]);
+gulp.task("default", ["watch", "runtests", "updaterequirements"])
