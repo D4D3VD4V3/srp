329089b8ce8f490114d9c9087de12fbff72b66cc
D4D3VD4V3 <dev.davidsundararaj@gmail.com>
2017-09-23 16:19:27 +0530

    .dockerignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require("gulp")
 var gshell = require("gulp-shell")
+var rename = require("gulp-rename")
 
 //gulp.task("activatevenv", gshell.task(["source ~/.virtualenvs/venv/bin/activate"]))
 gulp.task("runtests", gshell.task(["pytest"]))
 gulp.task("updaterequirements", gshell.task(["pip freeze > requirements.txt"]))
 
+gulp.task("updatedockerignore", function(){
+    gulp.src(".gitignore")
+    .pipe(rename("./.dockerignore"))
+    .pipe(gulp.dest("./"))})
+
 gulp.task("watch", function(){
     gulp.watch(["./app/**/*.py"], ["runtests"])})
 
-gulp.task("default", ["watch", "runtests", "updaterequirements"])
+gulp.task("default", ["watch", "runtests", "updaterequirements", "updatedockerignore"])
